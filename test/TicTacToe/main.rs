f83fab33077dc7e78d2c149896743f37bc3a152c
JpayeTei <root@LAPTOP-7C1IC5MD.localdomain>
2025-12-10 22:39:08 +0545

    using mut to cmmit a variable

---
+++ test/TicTacToe/main.rs
@@ -0,0 +1,8 @@
+//mut use garyo vane matra variable ko value change garna milxa natra mildaina
+fn main(){
+	let mut x = 5;
+	x = 10;
+	println!("{}", x);
+	println!("Hello World");
+	print!("yeslechai k garxa?");
+}
